d2c7373afd029f119ce20b05a0b02e1ba2560c2e
Munzir Zafar <mzafar7@gatech.edu>
2018-11-14 22:11:39 -0500

    Loads the robot successfully and simulates under the influence of gravity

--- include/load_objects.h
+++ include/load_objects.h
@@ -1,4 +1,76 @@
-#include <dart/dart.hpp>
+/*
+ * Copyright (c) 2018, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name of the Georgia Tech Research Corporation nor
+ *       the names of its contributors may be used to endorse or
+ *       promote products derived from this software without specific
+ *       prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GEORGIA TECH RESEARCH CORPORATION ''AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GEORGIA
+ * TECH RESEARCH CORPORATION BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
-dart::dynamics::SkeletonPtr createFloor();
-dart::dynamics::SkeletonPtr createKrang();
+/**
+ * @file load_objects.h
+ * @author Munzir Zafar
+ * @date Nov 14, 2018
+ * @brief Header for load_objects.cpp that loads objects into krang
+ * simulation
+ */
+
+#ifndef KRANG_SIMULATION_LOAD_OBJECTS_H_
+#define KRANG_SIMULATION_LOAD_OBJECTS_H_
+
+#include <dart/dart.hpp>
+#include <Eigen/Eigen>
+
+#include "sim_config.h"
+
+// Given heading and base angle of krang, this function returns the
+// angle axis representation for base frame orientation accepted by
+// DART
+Eigen::AngleAxisd GetKrangBaseAngleAxis(const double& headingInit,
+                                         const double& qBaseInit);
+
+// Given all the initial pose parameters, sets the positions of krang
+template <typename T>
+void SetKrangInitPos(const T& params, dart::dynamics::SkeletonPtr krang) {
+  Eigen::AngleAxisd aa;
+  aa = GetKrangBaseAngleAxis(params.headingInit, params.qBaseInit);
+  Eigen::Matrix<double, 25, 1> q;
+  q << aa.angle() * aa.axis(), params.xyzInit, params.qLWheelInit,
+      params.qRWheelInit, params.qWaistInit, params.qTorsoInit,
+      params.qKinectInit, params.qLeftArmInit, params.qRightArmInit;
+  krang->setPositions(q);
+}
+// Create a floor
+dart::dynamics::SkeletonPtr CreateFloor();
+
+// Create Krang with the initial pose that is specified by params
+// Path to krang's urdf is also to be specified in params
+dart::dynamics::SkeletonPtr CreateKrang(SimConfig& params);
+
+#endif // KRANG_SIMULATION_LOAD_OBJECTS_H_
