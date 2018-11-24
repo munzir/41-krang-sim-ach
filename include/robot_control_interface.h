42e813e59815b5e693175c136c68deebfc64a02d
Munzir Zafar <mzafar7@gatech.edu>
2018-11-23 19:06:35 -0500

    began working on robot control interface class. work in progress

---
+++ include/robot_control_interface.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name of the Georgia Tech Research Corporation nor
+ *       the names of its contributors may be used to endorse or
+ *       promote products derived from this software without specific
+ *       prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GEORGIA TECH RESEARCH CORPORATION ''AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GEORGIA
+ * TECH RESEARCH CORPORATION BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * @file robot_control_interface.h
+ * @author Munzir Zafar
+ * @date Nov 23, 2018
+ * @brief Header for RobotControlInterface class that is a composite of
+ * MotorGroup and SensorGroup objects to allow external interface to access
+ * robot states and control robot actuators
+ */
+
+#ifndef KRANG_SIMULATION_ROBOT_CONTROL_INTERFACE_H_
+#define KRANG_SIMULATION_ROBOT_CONTROL_INTERFACE_H_
+
+#include <dart/dart.hpp>  // dart::dynamics
+
+class RobotControlInterface {
+ public:
+  RobotControlInterface(dart::dynamics::SkeletonPtr robot,
+                        char* motor_config_file, char* interface_config_file);
+  void Destroy();
+  ~RobotControlInterface() { Destroy(); }
+
+  void Run();
+}
+
+#endif  // KRANG_SIMULATION_ROBOT_CONTROL_INTERFACE_H_
+
