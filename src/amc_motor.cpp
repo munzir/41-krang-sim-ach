15110858b6f8b39a598abeff586e9d48fe7204d5
Munzir Zafar <mzafar7@gatech.edu>
2018-11-27 08:07:07 -0500

    all coding done. and it compiles. not tested yet

--- src/amc_motor.cpp
+++ src/amc_motor.cpp
@@ -42,19 +42,146 @@
 
 #include "amc_motor.h"
 
-#include <dart/dart.hpp>  // dart::dynamics::
-#include <string>         // std::string
+#include <assert.h>
+#include <config4cpp/Configuration.h>  // config4cpp::Configuration
+#include <dart/dart.hpp>               // dart::dynamics::
+#include <iostream>                    // std::cerr
+#include <sstream>                     // istringstream
+#include <string>                      // std::string
 
 AmcMotor::AmcMotor(dart::dynamics::SkeletonPtr robot, std::string& joint_name,
-                   const char* motor_config_file) {}
-void AmcMotor::Update() {}
+                   const char* motor_config_file) {
+  joint_ = robot->getJoint(joint_name);
+
+  // Get the motor model from the motor_config_file
+  std::string motor_make, motor_model;
+  config4cpp::Configuration* cfg = config4cpp::Configuration::create();
+  const char* scope = "";
+  try {
+    cfg->parse(motor_config_file);
+    motor_make =
+        std::string(cfg->lookupString(scope, (joint_name + "_make").c_str()));
+    motor_model =
+        std::string(cfg->lookupString(scope, (joint_name + "_model").c_str()));
+  } catch (const config4cpp::ConfigurationException& ex) {
+    std::cerr << ex.c_str() << std::endl;
+    cfg->destroy();
+    assert(false && "Problem reading motor model from motor config file");
+  }
+
+  // Read parameters from file
+  // TODO: How to avoid the hardcoded path to cfg/amc folder?
+  ReadParams(("../cfg/" + motor_make + "/" + motor_model + ".cfg").c_str());
+
+  const double Newton_meters_per_pound_inch = 0.11298482933333;
+  const double radians_per_second_per_rpm = 0.104719755;
+  joint_->setForceUpperLimit(0,
+                             max_output_torque_ * Newton_meters_per_pound_inch);
+  joint_->setForceLowerLimit(
+      0, -max_output_torque_ * Newton_meters_per_pound_inch);
+  joint_->setVelocityUpperLimit(
+      0, 1.5 * rated_speed_ * radians_per_second_per_rpm);
+  joint_->setVelocityLowerLimit(
+      0, -1.5 * rated_speed_ * radians_per_second_per_rpm);
+  joint_->setDampingCoefficient(0, viscous_friction_);
+
+  CurrentCmd(0.0);
+}
+
+void AmcMotor::ReadParams(const char* motor_param_file) {
+  config4cpp::Configuration* cfg = config4cpp::Configuration::create();
+  const char* scope = "";
+  const char* str;
+  std::istringstream stream;
+  std::cout << "Reading parameters from " << std::string(motor_param_file)
+            << " ... " << std::endl;
+  try {
+    cfg->parse(motor_param_file);
+
+    rated_voltage_ = cfg->lookupFloat(scope, "rated_voltage");
+    std::cout << "rated_voltage: " << rated_voltage_ << std::endl;
+
+    rated_speed_ = cfg->lookupFloat(scope, "rated_speed");
+    std::cout << "rated_speed: " << rated_speed_ << std::endl;
+
+    rated_torque_ = cfg->lookupFloat(scope, "rated_torque");
+    std::cout << "rated_torque: " << rated_torque_ << std::endl;
+
+    rated_power_ = cfg->lookupFloat(scope, "rated_power");
+    std::cout << "rated_power: " << rated_power_ << std::endl;
+
+    rated_current_ = cfg->lookupFloat(scope, "rated_current");
+    std::cout << "rated_current: " << rated_current_ << std::endl;
+
+    line_to_line_resistance_ =
+        cfg->lookupFloat(scope, "line_to_line_resistance");
+    std::cout << "line_to_line_resistance: " << line_to_line_resistance_
+              << std::endl;
+
+    line_to_line_inductance_ =
+        cfg->lookupFloat(scope, "line_to_line_inductance");
+    std::cout << "line_to_line_inductance: " << line_to_line_inductance_
+              << std::endl;
+
+    torque_constant_ = cfg->lookupFloat(scope, "torque_constant");
+    std::cout << "torque_constant: " << torque_constant_ << std::endl;
+
+    back_emf_voltage_ = cfg->lookupFloat(scope, "back_emf_voltage");
+    std::cout << "back_emf_voltage: " << back_emf_voltage_ << std::endl;
+
+    rotor_inertia_ = cfg->lookupFloat(scope, "rotor_inertia");
+    std::cout << "rotor_inertia: " << rotor_inertia_ << std::endl;
+
+    gear_ratio_ = cfg->lookupFloat(scope, "gear_ratio");
+    std::cout << "gear_ratio: " << gear_ratio_ << std::endl;
+
+    rated_output_torque_ = cfg->lookupFloat(scope, "rated_output_torque");
+    std::cout << "rated_output_torque: " << rated_output_torque_ << std::endl;
+
+    stages_ = cfg->lookupFloat(scope, "stages");
+    std::cout << "stages: " << stages_ << std::endl;
+
+    max_backlash_ = cfg->lookupFloat(scope, "max_backlash");
+    std::cout << "max_backlash: " << max_backlash_ << std::endl;
+
+    viscous_friction_ = cfg->lookupFloat(scope, "viscous_friction");
+    std::cout << "viscous_friction: " << viscous_friction_ << std::endl;
+
+  } catch (const config4cpp::ConfigurationException& ex) {
+    std::cerr << ex.c_str() << std::endl;
+    cfg->destroy();
+    assert(false && "Problem reading motor params");
+  }
+}
+void AmcMotor::Update() {
+  const double Newton_meters_per_ounce_inch = 0.00706155183333;
+  double torque = reference_current_ *
+                  (Newton_meters_per_ounce_inch * torque_constant_) *
+                  gear_ratio_;
+  joint_->setForce(0, torque);
+}
 void AmcMotor::Destroy() {}
+
 void AmcMotor::Lock() {}
+
 void AmcMotor::Unlock() {}
+
 void AmcMotor::PositionCmd(double val) {}
 void AmcMotor::VelocityCmd(double val) {}
-void AmcMotor::CurrentCmd(double val) {}
-double AmcMotor::GetPosition() {return 0.0;}
-double AmcMotor::GetVelocity() {return 0.0;}
-double AmcMotor::GetCurrent() {return 0.0;}
-std::string AmcMotor::GetMotorType() { return "amc";}
+
+void AmcMotor::CurrentCmd(double val) {
+  reference_current_ = val;
+  joint_->setActuatorType(dart::dynamics::Joint::ActuatorType::FORCE);
+}
+
+double AmcMotor::GetPosition() { return joint_->getPosition(0); }
+
+double AmcMotor::GetVelocity() { return joint_->getVelocity(0); }
+
+double AmcMotor::GetCurrent() {
+  const double Newton_meters_per_ounce_inch = 0.00706155183333;
+  return (joint_->getForce(0) /
+          (Newton_meters_per_ounce_inch * torque_constant_) / gear_ratio_);
+}
+
+std::string AmcMotor::GetMotorType() { return "amc"; }
--- src/waist_motor.cpp
+++ src/waist_motor.cpp
@@ -42,19 +42,179 @@
 
 #include "waist_motor.h"
 
-#include <dart/dart.hpp>  // dart::dynamics::
-#include <string>         // std::string
+#include <assert.h>
+#include <config4cpp/Configuration.h>  // config4cpp::Configuration
+#include <dart/dart.hpp>               // dart::dynamics::
+#include <iostream>                    // std::cerr
+#include <sstream>                     // istringstream
+#include <string>                      // std::string
 
 WaistMotor::WaistMotor(dart::dynamics::SkeletonPtr robot,
-                       std::string& joint_name, const char* motor_config_file) {}
-void WaistMotor::Update() {}
+                         std::string& joint_name,
+                         const char* motor_config_file) {
+  joint_ = robot->getJoint(joint_name);
+
+  // Get the motor model from the motor_config_file
+  std::string motor_make, motor_model;
+  config4cpp::Configuration* cfg = config4cpp::Configuration::create();
+  const char* scope = "";
+  try {
+    cfg->parse(motor_config_file);
+    motor_make =
+        std::string(cfg->lookupString(scope, (joint_name + "_make").c_str()));
+    motor_model =
+        std::string(cfg->lookupString(scope, (joint_name + "_model").c_str()));
+  } catch (const config4cpp::ConfigurationException& ex) {
+    std::cerr << ex.c_str() << std::endl;
+    cfg->destroy();
+    assert(false && "Problem reading motor model from motor config file");
+  }
+
+  // Read parameters from file
+  // TODO: How to avoid the hardcoded path to cfg/waist folder?
+  ReadParams(("../cfg/" + motor_make + "/" + motor_model + ".cfg").c_str());
+
+  joint_->setForceUpperLimit(0, peak_torque_);
+  joint_->setForceLowerLimit(0, -peak_torque_);
+  joint_->setVelocityUpperLimit(0, max_angular_velocity_);
+  joint_->setVelocityLowerLimit(0, -max_angular_velocity_);
+  joint_->setAccelerationUpperLimit(0, max_angular_acceleration_);
+  joint_->setAccelerationLowerLimit(0, -max_angular_acceleration_);
+  joint_->setCoulombFriction(0, coulomb_friction_);
+  joint_->setDampingCoefficient(0, viscous_friction_);
+
+  Lock();
+}
+
+void WaistMotor::ReadParams(const char* motor_param_file) {
+  config4cpp::Configuration* cfg = config4cpp::Configuration::create();
+  const char* scope = "";
+  const char* str;
+  std::istringstream stream;
+  std::cout << "Reading parameters from " << std::string(motor_param_file)
+            << " ... " << std::endl;
+  try {
+    cfg->parse(motor_param_file);
+
+    motor_power_ = cfg->lookupFloat(scope, "motor_power");
+    std::cout << "motor_power: " << motor_power_ << std::endl;
+
+    nominal_torque_ = cfg->lookupFloat(scope, "nominal_torque");
+    std::cout << "nominal_torque: " << nominal_torque_ << std::endl;
+
+    peak_torque_ = cfg->lookupFloat(scope, "peak_torque");
+    std::cout << "peak_torque: " << peak_torque_ << std::endl;
+
+    max_angular_velocity_ = cfg->lookupFloat(scope, "max_angular_velocity");
+    std::cout << "max_angular_velocity: " << max_angular_velocity_ << std::endl;
+
+    max_angular_acceleration_ =
+        cfg->lookupFloat(scope, "max_angular_acceleration");
+    std::cout << "max_angular_acceleration: " << max_angular_acceleration_
+              << std::endl;
+
+    gear_ratio_ = cfg->lookupFloat(scope, "gear_ratio");
+    std::cout << "gear_ratio: " << gear_ratio_ << std::endl;
+
+    nominal_power_current_ = cfg->lookupFloat(scope, "nominal_power_current");
+    std::cout << "nominal_power_current: " << nominal_power_current_
+              << std::endl;
+
+    max_current_ = cfg->lookupFloat(scope, "max_current");
+    std::cout << "max_current: " << max_current_ << std::endl;
+
+    coulomb_friction_ = cfg->lookupFloat(scope, "coulomb_friction");
+    std::cout << "coulomb_friction: " << coulomb_friction_ << std::endl;
+
+    viscous_friction_ = cfg->lookupFloat(scope, "viscous_friction");
+    std::cout << "viscous_friction: " << viscous_friction_ << std::endl;
+
+    str = cfg->lookupString(scope, "position_ctrl_pd_gains");
+    stream.str(str);
+    stream >> position_ctrl_gains_.Kp_;
+    stream >> position_ctrl_gains_.Kd_;
+    stream.clear();
+    std::cout << "position_ctrl_pd_gains: " << position_ctrl_gains_.Kp_ << ", "
+              << position_ctrl_gains_.Kd_ << std::endl;
+
+    str = cfg->lookupString(scope, "speed_ctrl_pd_gains");
+    stream.str(str);
+    stream >> speed_ctrl_gains_.Kp_;
+    stream >> speed_ctrl_gains_.Kd_;
+    stream.clear();
+    std::cout << "speed_ctrl_pd_gains: " << speed_ctrl_gains_.Kp_ << ", "
+              << speed_ctrl_gains_.Kd_ << std::endl;
+  } catch (const config4cpp::ConfigurationException& ex) {
+    std::cerr << ex.c_str() << std::endl;
+    cfg->destroy();
+    assert(false && "Problem reading motor params");
+  }
+}
+void WaistMotor::Update() {
+  switch (mode_) {
+    case kLocked: {
+      break;
+    }
+    case kPositionCtrl: {
+      double current_position = joint_->getPosition(0);
+      double current_speed = joint_->getVelocity(0);
+      double reference_speed = 0.0;
+      double torque =
+          -position_ctrl_gains_.Kp_ * (current_position - reference_position_) -
+          position_ctrl_gains_.Kd_ * (current_speed - reference_speed);
+      joint_->setForce(0, torque);
+      break;
+    }
+    case kVelocityCtrl: {
+      double current_speed = joint_->getVelocity(0);
+      double torque =
+          -speed_ctrl_gains_.Kd_ * (current_speed - reference_speed_);
+      joint_->setForce(0, torque);
+      break;
+    }
+    case kCurrentCtrl: {
+      double torque = reference_current_ * peak_torque_ / max_current_;
+      joint_->setForce(0, torque);
+      break;
+    }
+  }
+}
 void WaistMotor::Destroy() {}
-void WaistMotor::Lock() {}
-void WaistMotor::Unlock() {}
-void WaistMotor::PositionCmd(double val) {}
-void WaistMotor::VelocityCmd(double val) {}
-void WaistMotor::CurrentCmd(double val) {}
-double WaistMotor::GetPosition() {return 0.0;}
-double WaistMotor::GetVelocity() {return 0.0;}
-double  WaistMotor::GetCurrent() {return 0.0;}
-std::string WaistMotor::GetMotorType() {return "waist";}
+
+void WaistMotor::Lock() {
+  mode_ = kLocked;
+  joint_->setActuatorType(dart::dynamics::Joint::ActuatorType::LOCKED);
+}
+
+void WaistMotor::Unlock() {
+  mode_ = kCurrentCtrl;
+  reference_current_ = 0.0;
+  joint_->setActuatorType(dart::dynamics::Joint::ActuatorType::FORCE);
+}
+
+void WaistMotor::PositionCmd(double val) {
+  mode_ = kPositionCtrl;
+  reference_position_ = val;
+  joint_->setActuatorType(dart::dynamics::Joint::ActuatorType::FORCE);
+}
+void WaistMotor::VelocityCmd(double val) {
+  mode_ = kVelocityCtrl;
+  reference_speed_ = val;
+  joint_->setActuatorType(dart::dynamics::Joint::ActuatorType::FORCE);
+}
+
+void WaistMotor::CurrentCmd(double val) {
+  mode_ = kCurrentCtrl;
+  reference_current_ = val;
+  joint_->setActuatorType(dart::dynamics::Joint::ActuatorType::FORCE);
+}
+
+double WaistMotor::GetPosition() { return joint_->getPosition(0); }
+
+double WaistMotor::GetVelocity() { return joint_->getVelocity(0); }
+
+double WaistMotor::GetCurrent() {
+  return (joint_->getForce(0) * max_current_ / peak_torque_);
+}
+
+std::string WaistMotor::GetMotorType() { return "waist"; }
